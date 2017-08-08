aaaf10745cf6efdf3374ef5ebbae67599edc0521
Rachael Serur <rachaelse@gmail.com>
2017-08-07 23:31:43 -0400

    Force SSL.

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,7 @@ config :elixir_with_love, ElixirWithLove.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "http", host: "www.elixir-with-love.com",
     port: 443],
-  # force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
